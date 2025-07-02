6c7a97763a34fa4f4449dcaabe0cdfcf4612bacf
Lu Semprebon <111984285+Herdt7@users.noreply.github.com>
2025-07-02 13:44:43 -0300

    Update difference_of_unsigned_inputs.vhdl

--- difference_of_unsigned_inputs.vhdl
+++ difference_of_unsigned_inputs.vhdl
@@ -3,17 +3,19 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY difference_of_unsigned_inputs IS -- declaração da entidade
+
+        generic (
+        N : positive -- número de bits das entradas e da saída
+    );
     PORT (
-        A: IN  UNSIGNED(7 DOWNTO 0);  -- Entrada A, valor sem sinal de 8 bits (0,...,255).
-        B: IN  UNSIGNED(7 DOWNTO 0);  -- Entrada B, valor sem sinal de 8 bits (0,...,255).
-        S: OUT SIGNED(8 DOWNTO 0) -- Saída S, valor com sinal em 9 bits representando A-B.
+        A: IN  UNSIGNED(N-1 DOWNTO 0);  -- Entrada A, valor sem sinal de 8 bits (0,...,255).
+        B: IN  UNSIGNED(N-1 DOWNTO 0);  -- Entrada B, valor sem sinal de 8 bits (0,...,255).
+        S: OUT SIGNED(N DOWNTO 0) -- Saída S, valor com sinal em 9 bits representando A-B.
     );
 END difference_of_unsigned_inputs;
 
 
--- Não alterar o nome da arquitetura!
 ARCHITECTURE arch OF difference_of_unsigned_inputs IS
-    -- Se precisar, podes adicionar declarações aqui (remova este comentário).
 BEGIN
-    s <= signed(resize(unsigned(A), 9) - (resize(unsigned(B),9)));
+    s <= signed(resize(unsigned(A), N+1) - (resize(unsigned(B),N+1)));
 END arch;
